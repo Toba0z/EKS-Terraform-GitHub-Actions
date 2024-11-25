3b066d79bedcc16116dc20e05141ddf9e7ce8379
Toba0z <tobgabby@aol.com>
2024-11-24 19:14:19 -0800

    change backend files

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1122"
+    bucket         = "my-ews-baket3"
     region         = "us-east-2"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "dynatbname"
     encrypt        = true
   }
 }
